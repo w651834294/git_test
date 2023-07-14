ab8a392265bc35d5b541195aac829f50e1612827
fufu <wafsb1314@gmail.com>
2023-07-14 21:56:37 +0800

    11

--- service/service_msm/src/main/java/com/fu/yygh/msm/service/MsmService.java
+++
@@ -1,9 +0,0 @@
-package com.fu.yygh.msm.service;
-
-import com.fu.yygh.vo.msm.MsmVo;
-
-public interface MsmService {
-    boolean sendCode(String phone);
-
-    void send(MsmVo msmVo);
-}
--- service/service_orders/src/main/java/com/fu/yygh/orders/mapper/PaymentMapper.java
+++
@@ -1,7 +0,0 @@
-package com.fu.yygh.orders.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.fu.yygh.model.order.PaymentInfo;
-
-public interface PaymentMapper extends BaseMapper<PaymentInfo> {
-}
